9cba468443fe32bd7991109b049056114e06e548
John Rogee Turqueza <johnrogeeturqueza21@gmail.com>
2024-07-21 22:22:14 +0800

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/assets/css/**/*.{css}",
-    "./public/assets/js/*.{js}",
-    "./app/views/**/*.{php}",
+    "./public/assets/css/**/*.css",
+    "./public/assets/js/*.js",
+    "./app/views/**/*.php",
   ],
   theme: {
     extend: {},
