e51d72c091e6ef5b60e17422310abc0f2d922bee
John Rogee Turqueza <johnrogeeturqueza21@gmail.com>
2024-07-22 17:18:38 +0800

    zealia db

--- app/model/config.php
+++ app/model/config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'mbti',
+        'dbname' => 'zealia',
         'charset' => 'utf8mb4'
     ]
     // ,
