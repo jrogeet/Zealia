db05a812db42acbcb7d2e3d69a05e5f2124680b8
John Rogee Turqueza <johnrogeeturqueza21@gmail.com>
2024-11-25 09:00:06 +0800

    loading visual update

--- app/views/rooms/room.view.php
+++ app/views/rooms/room.view.php
@@ -20,8 +20,11 @@
         <!-- Loading Indicator -->
         <div id="loadingIndicator" class="fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-75">
             <div class="flex flex-col items-center">
-                <div class="w-12 h-12 mb-4 border-4 border-t-4 border-blue-500 rounded-full animate-spin"></div>
-                <p class="text-xl font-satoshimed text-blue3">Loading...</p>
+                <!-- <div class="w-12 h-12 mb-4 border-4 border-t-4 border-blue-500 rounded-full animate-spin"></div>
+                <p class="text-xl font-satoshimed text-blue3">Loading...</p> -->
+                <div class="w-28 h-28 loader">
+                    
+                </div> 
             </div>
         </div>
     
